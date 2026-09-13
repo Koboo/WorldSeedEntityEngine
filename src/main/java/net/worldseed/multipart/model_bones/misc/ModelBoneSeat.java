b4392b750f3d795799a595e97fe1f960393a37b6
Koboo <admin@koboo.eu>
2026-09-13 15:44:01 +0200

    Implement boolean value rotateWithModel in ModelBoneSeat

--- src/main/java/net/worldseed/multipart/model_bones/misc/ModelBoneSeat.java
+++ src/main/java/net/worldseed/multipart/model_bones/misc/ModelBoneSeat.java
@@ -22,6 +22,8 @@ import java.util.concurrent.CompletableFuture;
 
 public class ModelBoneSeat extends ModelBoneImpl implements RideableBone {
 
+    private boolean rotateWithModel = false;
+
     public ModelBoneSeat(Point pivot, String name, Point rotation, GenericModel model, float scale) {
         super(pivot, name, rotation, model, scale);
 
@@ -137,11 +139,26 @@ public class ModelBoneSeat extends ModelBoneImpl implements RideableBone {
         this.children.forEach(ModelBone::draw);
         if (this.offset == null) return;
 
-        Pos found = calculatePosition();
+        Pos target = calculatePosition();
+        if(!rotateWithModel) {
+            Pos standPos = stand.getPosition();
+            stand.teleport(new Pos(
+                    target.x(), target.y(), target.z(),
+                    standPos.yaw(), standPos.pitch()
+            ));
+            return;
+        }
+        stand.teleport(target);
+    }
 
-        // TODO: needed by minestom?
-        stand.setView(found.yaw(), found.pitch());
-        stand.teleport(found);
+    @Override
+    public void setRotateWithModel(boolean rotateWithModel) {
+        this.rotateWithModel = rotateWithModel;
+    }
+
+    @Override
+    public boolean isRotateWithModel() {
+        return rotateWithModel;
     }
 
     @Override
